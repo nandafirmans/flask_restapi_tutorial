a61eb33fbff24b16f4f97d03fd8deb53fd4fed88
nanda firmansyah <nandafirmans@gmail.com>
2019-01-01 09:39:50 +0700

    fixing deployment error

--- app.py
+++ app.py
@@ -11,13 +11,6 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "nanda"
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)
 api = Api(app)
 
@@ -28,7 +21,4 @@ api.add_resource(StoreListResource, "/stores")
 api.add_resource(UserResource, "/user")
 
 if __name__ == "__main__":
-    from db import db
-
-    db.init_app(app)
     app.run(port=5000, debug=True)
